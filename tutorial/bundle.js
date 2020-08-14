c5bb3ca8e7bf8d52ad63e491d67b581e78664c95
DenarciKowatski <kowatskidenarci@gmail.com>
2020-08-14 16:25:34 -0300

    final de modulo

--- tutorial/bundle.js
+++ tutorial/bundle.js
@@ -1,5 +1,9 @@
 "use strict";
 
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
 /*class List{
     constructor(){
         this.data = [];
@@ -54,9 +58,72 @@ function teste(x){
         console.log(x,y);
     }
 }
-teste(10);*/
-var array = [1, 2, 4, 5, 6];
-var newAre = array.map(function (item, index) {
-  return item * index;
+teste(10);
+
+const array = [1,2,3,4,5,6,];
+
+const newAre = array.map(function(item, index){
+    return item * index;//percorre todo o array
 });
 console.log(array, newAre);
+
+
+const sum = array. reduce(function(total, next){
+    return total + next;//reduz o array a um unico valor
+});
+console.log(sum);
+
+const filter = array.filter(function(item){
+    return item % 2 === 0;//filtra valores da array
+});
+
+console.log(filter);
+
+const find = array.find(function(item){
+    return item === 4;
+});
+
+console.log(find);
+const array = [1,2,3,4,5,6,];
+
+const newAre = array.map(item => item * 2);
+console.log(array, newAre);
+
+const soma = (a = 3, b = 5)=> a + b;
+console.log(soma(1));
+
+const usuário = {
+    nome: 'Denarci',
+    idade: 24,
+    endereço:{
+        cidade: 'concordia',
+        estado:'SC'
+    },
+};
+
+console.log(usuário);
+
+const { nome, idade, endereço:{ cidade}} = usuário;
+console.log(nome, idade, cidade);*/
+//REST
+var usuário = {
+  nome: 'Denarci',
+  idade: 24,
+  endereço: {
+    cidade: 'concordia',
+    estado: 'SC'
+  }
+};
+
+var nome = usuário.nome,
+    resto = _objectWithoutProperties(usuário, ["nome"]);
+
+console.log(nome);
+console.log(resto);
+var array = [1, 2, 3, 45];
+var a = array[0],
+    b = array[1],
+    c = array.slice(2);
+console.log(a);
+console.log(b);
+console.log(c);
--- tutorial/main.js
+++ tutorial/main.js
@@ -52,13 +52,72 @@ function teste(x){
         console.log(x,y);
     }
 }
-teste(10);*/
+teste(10);
 
-const array = [1,2,4,5,6,];
+const array = [1,2,3,4,5,6,];
 
 const newAre = array.map(function(item, index){
-    return item * index;
+    return item * index;//percorre todo o array
 });
 console.log(array, newAre);
 
 
+const sum = array. reduce(function(total, next){
+    return total + next;//reduz o array a um unico valor
+});
+console.log(sum);
+
+const filter = array.filter(function(item){
+    return item % 2 === 0;//filtra valores da array
+});
+
+console.log(filter);
+
+const find = array.find(function(item){
+    return item === 4;
+});
+
+console.log(find);
+const array = [1,2,3,4,5,6,];
+
+const newAre = array.map(item => item * 2);
+console.log(array, newAre);
+
+const soma = (a = 3, b = 5)=> a + b;
+console.log(soma(1));
+
+const usuário = {
+    nome: 'Denarci',
+    idade: 24,
+    endereço:{
+        cidade: 'concordia',
+        estado:'SC'
+    },
+};
+
+console.log(usuário);
+
+const { nome, idade, endereço:{ cidade}} = usuário;
+console.log(nome, idade, cidade);*/
+
+
+//REST
+
+const usuário = {
+    nome: 'Denarci',
+    idade: 24,
+    endereço:{
+        cidade: 'concordia',
+        estado:'SC'
+    },
+};
+
+const {nome, ...resto} = usuário;
+console.log(nome);
+console.log(resto);
+
+const array = [1,2,3,45];
+const [a, b, ...c] = array;
+console.log(a);
+console.log(b);
+console.log(c);
