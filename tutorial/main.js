e48b7ef7ee5edc995a170eff4c5e8f7351f512d2
DenarciKowatski <kowatskidenarci@gmail.com>
2020-08-14 11:52:15 -0300

    Inicio de modulo

---
+++ tutorial/main.js
@@ -0,0 +1,55 @@
+/*class List{
+    constructor(){
+        this.data = [];
+    }
+    add(data){
+        this.data.push(data);
+        console.log(this.data)
+    }
+}
+
+class TodoList extends List{//extends da de herança as propriades da função pai
+    constructor(){
+        super();//chama a função constructor do pai
+
+        this.user = 'Dena'
+    }
+    mostraUser(){
+        console.log(this.user);
+    }
+
+}
+
+var MyList = new TodoList();
+
+document.getElementById('newtodo').onclick = function(){
+    MyList.add('novo todo');
+}
+
+MyList.mostraUser();
+
+class Calcula{//metodos estaticos 
+    constructor(){
+        this.todos = [];
+    }
+    static soma(a,b){//
+        
+        return a + b;
+    }
+}
+console.log(Calcula.soma(7,6));
+
+const a = 1; //variavel sempre constante
+a = 3;
+
+const user = {nome: 'Dena'};
+user.nome = 'Cleiton';//mutar a variavel
+
+function teste(x){
+    let y = 2
+
+    if(x > 5){
+        console.log(x,y);
+    }
+}
+teste(10);*/
